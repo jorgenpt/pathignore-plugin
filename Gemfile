d40056a643ed5a23dfa5f9839f356c37343a0907
Jørgen P. Tjernø <jtjerno@mylookout.com>
2011-10-25 19:01:28 -0700

    Update to slightly more working plugin runtime etc

--- Gemfile
+++ Gemfile
@@ -2,4 +2,8 @@
 source :rubygems
 
 # gem "jenkins-plugin-runtime", "~> 0.1.8"
-gem "jenkins-plugin-runtime", :git => 'git://github.com/jorgenpt/jenkins-plugin-runtime.git'
+gem "jenkins-plugin-runtime",
+    :git => 'git://github.com/jorgenpt/jenkins-plugin-runtime.git',
+    :branch => 'bump-version'
+
+gem "jenkins-plugin", "~> 0.1.13"
--- models/pathfilter_wrapper.rb
+++ models/pathfilter_wrapper.rb
@@ -1,14 +1,23 @@
 class PathfilterWrapper < Jenkins::Tasks::BuildWrapper
+  display_name "Pathfilter"
+
   # Called some time before the build is to start.
   def setup(build, launcher, listener, env)
     listener.info "build will start"
+    build.native.setResult(Java.hudson.model.Result::NOT_BUILT)
+
+    changeset = build.native.getChangeSet()
+    listener.info "Changeset: #{changeset}"
     begin
-      build.native.setResult(Java.hudson.model.Result::NOT_BUILT)
+      changeset.each {|change|
+        listener.info " Change: #{change}"
+        files = change.getAffectedFiles()
+        listener.info "  Affected: #{files}"
+      }
     rescue
-      listener.info "FAIL #{$!}"
     end
 
-    false
+    build.halt("Stopping you")
   end
 
   # Called some time when the build is finished.
